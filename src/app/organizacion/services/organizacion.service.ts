f3655051f7bbebc4ace3a6e73e9a4bf927f0def0
Unknown <ogasvega.israel@gmail.com>
2018-02-01 13:43:55 -0300

    Revert "style: organizando rutas"
    This reverts commit 9c4c03ccdd994573e5f8dce2b3966dfd0a601315.

--- src/app/organizacion/services/organizacion.service.ts
+++ src/app/organizacion/services/organizacion.service.ts
@@ -11,28 +11,30 @@ const httpOptions = {
 @Injectable()
 export class OrganizacionService {
 
-  domain: string = "http://localhost:10010/organizaciones";
+  domain: string = "http://localhost:10010";
 
   constructor(private http: HttpClient) { }
 
   getOrganizaciones()  {
-    return this.http.get(`${this.domain}`)
+    //console.log(this.http.get<Organizacion[]>(`${this.domain}/organizaciones`));
+    return this.http.get(`${this.domain}/organizaciones`)
   }
 
   getOrganizacion(id: String) {
-    const url = `${this.domain}/${id}`;
+    const url = `${this.domain}/organizaciones/${id}`;
+    //console.log(this.http.get(url))
     return this.http.get(url)
   }
 
   postOrganizacion(organizacion: Object) {
-    return this.http.post(`${this.domain}`, organizacion)
+    return this.http.post(`${this.domain}/organizaciones`, organizacion)
   }
 
   updateOrganizacion(organizacion: Object, id: String) {
-    return this.http.put(`${this.domain}/${id}`, organizacion, httpOptions)
+    return this.http.put(`${this.domain}/organizaciones/${id}`, organizacion, httpOptions)
   }
 
   deleteOrganizacion(id: String) {
-    return this.http.delete(`${this.domain}/${id}`)
+    return this.http.delete(`${this.domain}/organizaciones/${id}`)
   }
 }
